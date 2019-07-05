7955d7930e349ba05fe485bfe96aa211b4c88b6e
Karol Wajs <wajskarol@gmail.com>
2019-07-05 07:57:50 +0200

    Reworked moduleFactory

--- src/Vehicles/ModuleFactory.hpp
+++ src/Vehicles/ModuleFactory.hpp
@@ -16,85 +16,37 @@ class Yaml;
 
 class ModuleFactory
 {
+private:
     VehicleEquipment &m_vehicleEq;
     PhysicalWorld &physics;
     btVector3 startPosition;
     int weaponId {0};
+    std::map<std::string, std::function<std::shared_ptr<IModule>(const Yaml&)>> m_moduleMap;
+
 
 public:
-    ModuleFactory(VehicleEquipment &eq, PhysicalWorld &physics, glm::vec4 startPosition)
-    : m_vehicleEq(eq), physics(physics), startPosition(convert(startPosition)) {}
-
     btRigidBody *vehicle;
 
-    std::shared_ptr<IMotor> createDieselMotor(const Yaml &cfg){
-        return nullptr;
-    }
-    std::shared_ptr<IMotor> createElectricMotor(const Yaml &cfg){
-        return nullptr;
+    ModuleFactory(VehicleEquipment &eq, PhysicalWorld &physics, glm::vec4 startPosition)
+    : m_vehicleEq(eq), physics(physics), startPosition(convert(startPosition)) {
+        m_moduleMap["Hull"] = [this](const Yaml& cfg){ return std::make_shared<Base>(m_vehicleEq); };
+        m_moduleMap["TurretBase"] = [this](const Yaml& cfg){ return std::make_shared<Turret>(m_vehicleEq); };
+        m_moduleMap["TurretPart"] = [this](const Yaml& cfg){ return std::make_shared<TurretPart>(m_vehicleEq); };
+        m_moduleMap["Gun"] = [this](const Yaml& cfg){ return std::make_shared<Gun>(m_vehicleEq); };
+        m_moduleMap["Addon"] = [this](const Yaml& cfg){ return std::make_shared<Addon>(m_vehicleEq); };
+        m_moduleMap["LoosePart"] = [this](const Yaml& cfg){ return std::make_shared<Addon>(m_vehicleEq); };
+        m_moduleMap["Armor"] = [this](const Yaml& cfg){ return std::make_shared<Armor>(m_vehicleEq); };
     }
 
-    std::shared_ptr<IModule> createModule(const Yaml &cfg){
-        std::string className = cfg["Class"].string();
-
-        if(className == "Hull"){
-            return createBase(cfg);
-        }
-        else if(className == "TurretBase"){
-            return createTurret(cfg);
-        }
-        else if(className == "TurretPart"){
-            return std::make_shared<TurretPart>(m_vehicleEq);
-        }
-        else if(className == "Gun"){
-            return createAddon(cfg);
-        }
-        else if(className == "Addon"){
-            return createAddon(cfg);
-        }
-        else if(className == "LoosePart"){
-            return createLoosePart(cfg);
-        }
-        else if(className == "Armor"){
-            return createArmor(cfg);
+    std::shared_ptr<IModule> createModule(const Yaml& cfg){
+        auto type = cfg["Class"].string();
+        if(m_moduleMap.count(type) == 0){
+            console.error("No module of type", type);
+            return {};
         }
 
-        console.error("Module", cfg["Name"].string(), "has undefined type:", className);
-        return nullptr;
+        return m_moduleMap[type](cfg);
     }
-    std::shared_ptr<IModule> createBase(const Yaml &cfg){
-        auto ptr = std::make_shared<Base>(m_vehicleEq);
 
-        return ptr;
-    }
-    std::shared_ptr<IModule> createTurret(const Yaml &cfg){
-        auto ptr = std::make_shared<Turret>(m_vehicleEq);
 
-        return ptr;
-    }
-    std::shared_ptr<IModule> createAddon(const Yaml &cfg){
-        auto ptr = std::make_shared<Addon>(m_vehicleEq);
-
-        return ptr;
-    }
-    std::shared_ptr<IModule> createArmor(const Yaml &cfg){
-        auto ptr = std::make_shared<Armor>(m_vehicleEq);
-
-        return ptr;
-    }
-    std::shared_ptr<IModule> createPowerShield(const Yaml &cfg){
-        auto ptr = std::make_shared<Armor>(m_vehicleEq);
-
-        return ptr;
-    }
-    std::shared_ptr<IModule> createHeadlight(const Yaml &cfg){
-        auto ptr = std::make_shared<Headlight>(m_vehicleEq);
-
-        return ptr;
-    }
-    std::shared_ptr<IModule> createLoosePart(const Yaml &cfg){
-        auto ptr = std::make_shared<LoosePart>(m_vehicleEq);
-
-        return ptr;
-    }
 };
